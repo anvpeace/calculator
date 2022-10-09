d8b2b919893339a874c7e3ac56ae02450e0828ae
anvpeace <cruzpeacem@gmail.com>
2022-10-09 11:31:29 -0400

    first operand working with decimal

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-// First this calculator application will take the value of the button selected by the client 
 
-// Second the application will display the number selected by the client in the NumDisplay input field
+let displayNum = document.getElementById('numberInput');
+let firstOperand = document.getElementsByClassName('numberDisplay');
 
-const calculator = {
-    displayValue: 0,
-    operandOne: null,
-    isSecondOperand: false,
-    operator: null
+
+
+
+function display (val){
+
+    nums = displayNum.value += val
+
+    operandOne = String(nums)
+   
+    firstOperand.innerHtml = `${parseFloat(operandOne).toFixed(7)}`
+
+    console.log(operandOne)
+    console.log(firstOperand)
+
 }
 
-function displayUpdate (){
-    const display = document.querySelector(".numberInput")
+function decimal(dot){
 
-    display.value = calculator.displayValue
-}
-
-displayUpdate();
-
-
-
-const numbers = document.querySelector(".btnDisplay");
-
-numbers.addEventListener("click", (event) =>{
-    const {target} = event;
-
-    switch (target.match){
-        case !target.match:
-            break;
-        case target.classList.contains("operator"):
-            console.log("operator", target.value);
-            break;
-        case target.classList.contains("decimal"):
-            console.log("decimal", target.value);
-            break;
-        case target.classList.contains("all-clear"):
-            console.log("all-clear", target.value);
-            break;
-
+    if(!displayNum.value.includes(dot)){
+        displayNum.value += dot
     }
-    console.log("digit", target.value)
-})
+    // }else if(displayNum.value.includes(dot)){
+    //     console.log("decimal found")
+    //     console.log(firstOperand)
+
+    // }
+
+}
+
+// function decimal(dot){
+
+//     let firstOp = document.getElementsByClassName('numberDisplay');
+//     console.log(firstOp)
+
+//     if(!firstOp.includes(dot)){
+//         firstOp += val
+
+//     }
+// }
+
+
+ 
