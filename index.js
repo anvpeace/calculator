4ddd3645861e625579d2b634053f52c4d9090750
anvpeace <cruzpeacem@gmail.com>
2022-10-14 11:12:04 -0400

    calculations working clear buttong and sum display next

--- index.js
+++ index.js
@@ -1,46 +1,184 @@
 
-let displayNum = document.getElementById('numberInput');
-let firstOperand = document.getElementsByClassName('numberDisplay');
+var displayCurrentValue = document.getElementById('number-input');
+
+var resultDisplay= document.querySelector('.numberContainer')
+
+var numberOneDisplay = document.getElementById('operand-one');
+
+var operatorDisplay = document.getElementById('operator-display');
+
+var numberTwoDisplay = document.getElementById('operand-two');
+
+var numbers = document.querySelectorAll('.number');
+
+var operations = document.querySelectorAll('.operation');
+
+var equalsEl = document.querySelector('.equals');
+
+var total = document.getElementById('equation-total');
+
+
+
+let firstOperand = '';
+let operatorValue = '';
+let secondOperand = '';
+let result = null;
+let hasDot= false
+
+numbers.forEach(number =>{
+    number.addEventListener('click',(e)=>{
+        if(e.target.innerText === "." && !hasDot){
+            hasDot = true
+
+        }else if(e.target.innerText === "." && hasDot){
+            return;
+        }
+        
+        firstOperand = displayCurrentValue.value += e.target.innerText
 
 
 
 
-function display (val){
+    })
 
-    nums = displayNum.value += val
+})
 
-    operandOne = String(nums)
-   
-    firstOperand.innerHtml = `${parseFloat(operandOne).toFixed(7)}`
 
-    console.log(operandOne)
-    console.log(firstOperand)
+operations.forEach(operator =>{
+    operator.addEventListener("click", (e)=>{
+        displayCurrentValue.value = ""
+        numberOneDisplay.innerText = `${parseFloat(firstOperand).toFixed(2)}`
 
-}
+        if(!displayCurrentValue)return;
+        hasDot = false
+        // operatorDisplay.innerText= `${e.target.innerText}`
+        operatorValue = e.target.innerText
+        operatorDisplay.innerText = operatorValue
 
-function decimal(dot){
+        // console.log(operatorValue)
 
-    if(!displayNum.value.includes(dot)){
-        displayNum.value += dot
+       
+        // console.log(result)
+    })
+})
+
+equalsEl.addEventListener("click", (e) =>{
+    secondOperand = displayCurrentValue.value
+
+    numberTwoDisplay.innerText = `${parseFloat(secondOperand).toFixed(2)}`
+
+        displayCurrentValue.value = ""
+
+    if(firstOperand && secondOperand){
+        operation()
+
     }
-    // }else if(displayNum.value.includes(dot)){
-    //     console.log("decimal found")
-    //     console.log(firstOperand)
 
-    // }
 
+})
+
+function operation(){
+    opOne = Number(numberOneDisplay.innerText)
+    opTwo = Number(secondOperand)
+
+    switch(operatorValue){
+        case "+":
+           result= opOne + opTwo;
+           displayCurrentValue.innerText += result
+           console.log(result)
+           total.innerText = ` = ${result}`
+           break;
+
+        case "-":
+            result = opOne - opTwo;
+            console.log(result)
+            break;
+        case "÷":
+            result = opOne / opTwo;
+            console.log(result)
+            break;
+
+    }
 }
 
-// function decimal(dot){
 
-//     let firstOp = document.getElementsByClassName('numberDisplay');
-//     console.log(firstOp)
 
-//     if(!firstOp.includes(dot)){
-//         firstOp += val
 
-//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function secondOperand (val){
+
+
+//     // numsTwo = displayNum.value += val
+//     // operandTwo = String(numsTwo)
+
+//     // console.log(numsTwo)
+
+//     // operandTwo = String(numsTwo)
+
+//     // opTwoDisplay.innerText = operandTwo
+
+//     // displayNum.append(operandTwo)
 // }
-
 
  
